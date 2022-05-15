849fc2a18c08f4b5d0c69f191457b3377e7939c8
Abhijith V G <abhijithvg@gmail.com>
2022-05-15 22:59:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello from Java - my first update");
+    System.out.println("Hello from Java - my first update 2");
   }
 }
